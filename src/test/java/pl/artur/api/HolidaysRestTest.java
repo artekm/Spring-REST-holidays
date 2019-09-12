4b6f6f1e00b05e5eb7c31bf099829cd131391f6d
artekm <44265668+artekm@users.noreply.github.com>
2019-09-12 11:11:28 +0200

    updates

--- src/test/java/pl/artur/api/HolidaysRestTest.java
+++ src/test/java/pl/artur/api/HolidaysRestTest.java
@@ -1,6 +1,6 @@
 package pl.artur.api;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
